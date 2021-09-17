6597b9088285fc7ae6b23b7f0d77b3fb488e0af7
wiksell91 <victor.wiksell@gmail.com>
2021-09-17 13:05:48 +0200

    Start projekt Victor, Miranda o Joel

---
+++ src/main/java/com/ViMiJo/webservicemusicaccount/WebservicemusicaccountApplication.java
@@ -0,0 +1,13 @@
+package com.ViMiJo.webservicemusicaccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebservicemusicaccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebservicemusicaccountApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ViMiJo/webservicemusicaccount/WebservicemusicaccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ViMiJo.webservicemusicaccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebservicemusicaccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
