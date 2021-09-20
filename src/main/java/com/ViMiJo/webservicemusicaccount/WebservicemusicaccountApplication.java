a1cbc885d817daf6f1d6fe104efabb2167f6319b
wiksell91 <victor.wiksell@gmail.com>
2021-09-20 14:58:06 +0200

    Skapat Arraylist för befintliga användare samt lägga till nya användare Alla i gruppen

--- src/main/java/com/ViMiJo/webservicemusicaccount/WebservicemusicaccountApplication.java
+++ src/main/java/com/ViMiJo/webservicemusicaccount/WebservicemusicaccountApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WebservicemusicaccountApplication {
 
 	public static void main(String[] args) {
+
+
 		SpringApplication.run(WebservicemusicaccountApplication.class, args);
+
 	}
 }
