698b0c79e2225eb8f8e38a4608a60a47db436351
wiksell91 <victor.wiksell@gmail.com>
2021-09-23 10:21:35 +0200

    get, put, post o delete fixat samt kopplat mot databas

---
+++ src/main/java/com/ViMiJo/webservicemusicaccount/AccountuserAuto.java
@@ -0,0 +1,42 @@
+
+package com.ViMiJo.webservicemusicaccount;
+/*
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+
+@Configuration
+public class AccountuserAuto {
+
+    @Bean
+    CommandLineRunner commandLineRunner(AccountuserRepository repository){
+        return args -> {
+            Accountuser joel = new Accountuser(
+                    "JolleH",
+                    "Joel Holewa",
+                    "1234"
+            );
+            Accountuser miranda = new Accountuser(
+                    "MirreF",
+                    "Miranda Frisö",
+                    "4weqw"
+            );
+            Accountuser victor = new Accountuser(
+                    "VirreW",
+                    "Victor Wiksell",
+                    "7872"
+            );
+
+            repository.saveAll(
+                    List.of(joel, miranda, victor)
+            );
+
+        };
+    }
+
+}
+*/
---
+++ src/main/java/com/ViMiJo/webservicemusicaccount/AccountuserService.java
@@ -0,0 +1,79 @@
+package com.ViMiJo.webservicemusicaccount;
+
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+
+@Service
+public class AccountuserService {
+
+    private final AccountuserRepository accountuserRepository;
+
+    @Autowired
+    public AccountuserService(AccountuserRepository accountuserRepository)
+    {
+        this.accountuserRepository = accountuserRepository;
+    }
+
+    public List<Accountuser> getAccountusers(){
+        return accountuserRepository.findAll();
+    }
+
+    public void addNewAccountuser(Accountuser accountuser){
+        Optional<Accountuser> accountUserOptional =
+                accountuserRepository.findAccountuserByUserName(accountuser.getUserName());
+        if (accountUserOptional.isPresent()) {
+            throw new IllegalStateException("Sorry, användarnamet är upptaget");
+        }
+        accountuserRepository.save(accountuser);
+    }
+
+    public void deleteaccountuser(Long accoutuserId){
+        boolean exist = accountuserRepository.existsById(accoutuserId);
+        if (!exist){
+            throw new IllegalStateException(
+                    "Användare med id " + accoutuserId + " finns inte"
+            );
+        }
+        accountuserRepository.deleteById(accoutuserId);
+    }
+
+    @Transactional
+    public void updateAccountuser(Long accountuserId, String userName, String name, String passWord){
+        Accountuser accountuser = accountuserRepository.findById(accountuserId)
+                .orElseThrow(()-> new IllegalStateException(
+                        "Användare med id " + accountuserId + " finns inte"
+                ));
+        if(userName != null && userName.length()> 0 && !Objects.equals(accountuser.getUserName(), userName)){
+            Optional<Accountuser> accountuserOptional = accountuserRepository
+                    .findAccountuserByUserName(userName);
+            if (accountuserOptional.isPresent()){
+                throw new IllegalStateException("Sorry användarnamnet är upptaget, Hitt på nå nytt ba!");
+            }
+            accountuser.setUserName(userName);
+        }
+        if (name != null && name.length() > 0 && !Objects.equals(accountuser.getName(), name))
+        {
+            accountuser.setName(name);
+        }
+
+        if (passWord != null && passWord.length() > 0 && !Objects.equals(accountuser.getPassWord(), passWord))
+        {
+            accountuser.setPassWord(passWord);
+        }
+
+
+    }
+
+
+
+
+
+
+}
