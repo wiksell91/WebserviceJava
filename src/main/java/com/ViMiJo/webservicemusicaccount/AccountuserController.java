293eaf7745c2a2469521b2d2b4b992c992ccd0a8
wiksell91 <victor.wiksell@gmail.com>
2021-09-28 14:10:30 +0200

    Slutprodukt, lagt till bild o method för att visa bild miranda, victor o joel

--- src/main/java/com/ViMiJo/webservicemusicaccount/AccountuserController.java
+++ src/main/java/com/ViMiJo/webservicemusicaccount/AccountuserController.java
@@ -3,7 +3,11 @@ package com.ViMiJo.webservicemusicaccount;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.core.io.ClassPathResource;
+import org.hibernate.engine.jdbc.StreamUtils;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,12 +22,19 @@ public class AccountuserController {
         this.accountuserService = accountuserService;
     }
 
-
+    @RequestMapping(method = RequestMethod.GET,
+            produces = MediaType.IMAGE_JPEG_VALUE)
+    public void getImage(HttpServletResponse response) throws IOException {
+        var imgFile = new ClassPathResource("Image/LoginWindow.jpg");
+        response.setContentType(MediaType.IMAGE_JPEG_VALUE);
+        StreamUtils.copy(imgFile.getInputStream(), response.getOutputStream());
+    }
 
     //GET en användare
-    @GetMapping("{accountuserId}")
-    public Optional<Accountuser> oneUser(@PathVariable("accountuserId" ) Long accountuserId) {
-        return accountuserService.oneUser(accountuserId);
+    @GetMapping(value = "/user/{accountUserId}", produces = {MediaType.APPLICATION_JSON_VALUE,
+            MediaType.APPLICATION_XML_VALUE})
+    public Optional<Accountuser> oneUser(@PathVariable("accountUserId" ) Long accountUserId) {
+        return accountuserService.oneUser(accountUserId);
     }
 
     @GetMapping(path ="/users", produces = {MediaType.APPLICATION_XML_VALUE,
